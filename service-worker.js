fe1bf84fdc277ff52c2c2eca2c0e4a547bdf8f3a
srinivas <76845488+srinumis@users.noreply.github.com>
2026-03-13 07:52:59 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,26 @@
+// service-worker.js
+const CACHE_NAME = "cce-dashboard-v1";
+
+// cache essential files for offline
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
 self.addEventListener("install", event => {
   console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
