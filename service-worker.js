563dfa92f923d706f97bfb210179d949d5cf90de
srinivas <76845488+srinumis@users.noreply.github.com>
2026-03-13 07:30:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,30 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "marks-dashboard-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+  );
+
 });
